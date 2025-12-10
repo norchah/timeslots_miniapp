361e0b5e9faffe9b7f3e7bfa950d2c2e11fc1a03
norchah <norchah@mail.ru>
2025-12-10 12:19:38 +0200

    fix

---
+++ api/userApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import camelcaseKeys from "camelcase-keys";
+export class UserApi {
+  constructor() {
+    this.api = axios.create({
+      baseURL: 'https://norchah.ru/test/users/',
+      withCredentials: true,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  async login(initData) {
+    console.log(initData);
+    const res = await this.api.post('/auth/login/', {init_data: initData});
+    return camelcaseKeys(res.data, {deep: true});
+  }
+
+  async checkInitData(initData) {
+    const res = await this.api.post('/check', {init_data: initData});
+    return camelcaseKeys(res.data, {deep: true});
+  }
+}
