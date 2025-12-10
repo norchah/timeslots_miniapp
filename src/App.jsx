43dc0a360fcb1d5b8eb8aaacf987e86bd21e7e68
norchah <norchah@mail.ru>
2025-12-10 23:36:35 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ export default function App() {
 
   return (
     <div
-      className="m-auto max-w-[456px] flex flex-col items-center justify-center outline mt-[30px]"
+      className="m-auto max-w-[456px] flex flex-col items-center justify-center outline mt-[34px]"
       style={{paddingTop: `${safeTop}px`, paddingBottom: `${safeBottom}px`}}
     >
       <h1>Добро пожаловать в TimeSlots</h1>
