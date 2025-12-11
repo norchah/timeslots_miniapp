e5e1cc5b722c4bf8c382ee4369c3034998547127
norchah <norchah@mail.ru>
2025-12-11 16:17:18 +0200

    fix

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -8,13 +8,13 @@ export default function SettingsPage({navigate, tgData, user, safeTop, safeBotto
     <div>
       <h1>Настройки</h1>
       <p>Пользователь:</p>
+      <img className='w-[60px] h-[60px] rounded-full' src={user.photo_url} alt='avatar'/>
       <p>{user.username}</p>
       <p>{user.firstName}</p>
       <p>{user.lastName}</p>
       <p>{user.photoUrl}</p>
       <p>{user.displayName}</p>
       <p>{user.displayLastname}</p>
-      <p>{user.displayPhoto}</p>
     </div>
   );
 }
