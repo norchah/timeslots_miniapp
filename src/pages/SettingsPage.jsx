0fbe3c26cb40536088aa407956d6d7dda468d7f3
norchah <norchah@mail.ru>
2025-12-11 16:32:45 +0200

    fix

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -14,9 +14,9 @@ export default function SettingsPage({navigate, tgData, user, safeTop, safeBotto
 
   return (
     <div>
-      <h1 className='text-lg'>Настройки</h1>
+      <h1 className='text-l'>Настройки</h1>
       <p>Пользователь:</p>
-      <div className="flex flex-col jusify-bettwen align-center">
+      <div className="flex items-center justify-center flex-col">
         <img className='w-[80px] h-[80px] rounded-full' src={user.photoUrl} alt='avatar'/>
         <p>Имя пользователя: {getText(user.username)}</p>
         <p>Имя из телеграма: {getText(user.firstName)}</p>
