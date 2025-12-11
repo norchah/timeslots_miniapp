df29ff6a827ffec2db5234ff4f3253fa811f8b58
norchah <norchah@mail.ru>
2025-12-11 17:56:32 +0200

    fix

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -9,15 +9,19 @@ export default function SettingsPage({navigate, tgData, user, safeTop, safeBotto
 
 
   return (
-    <div className="outline w-full">
-      <h1 className='text-l align-center'>Настройки</h1>
-      <p>Пользователь:</p>
-      <div className="flex items-center justify-center flex-col">
-        <img className='w-[80px] h-[80px] rounded-full' src={photoUrl} alt='avatar'/>
-        <p>Имя пользователя: {username}</p>
-        <p>Имя: {name}</p>
-        <p>Фамилия: {lastname}</p>
-      </div>
+    <div className="outline flex flex-col justify-center items-center w-full">
+      <header className="outline outline-red-400">
+        <h1 className='text-2xl'>Настройки</h1>
+      </header>
+      <main className="outline outline-pink-500">
+        <p>Пользователь:</p>
+        <div className="flex items-center justify-center flex-col outline outline-sky-400">
+          <img className='w-[80px] h-[80px] rounded-full' src={photoUrl} alt='avatar'/>
+          <p>Имя пользователя: {username}</p>
+          <p>Имя: {name}</p>
+          <p>Фамилия: {lastname}</p>
+        </div>
+      </main>
     </div>
   );
 }
