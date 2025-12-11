4f716441fbe9cebab54c5f95cef8d1ab89205032
norchah <norchah@mail.ru>
2025-12-11 16:31:33 +0200

    fix

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -18,11 +18,11 @@ export default function SettingsPage({navigate, tgData, user, safeTop, safeBotto
       <p>Пользователь:</p>
       <div className="flex flex-col jusify-bettwen align-center">
         <img className='w-[80px] h-[80px] rounded-full' src={user.photoUrl} alt='avatar'/>
-        <p>Имя пользователя: getText({user.username})</p>
-        <p>Имя из телеграма: getText({user.firstName})</p>
-        <p>Фамилия из телеграма: getText({user.lastName})</p>
-        <p>Отображаемое имя в приложении: getText({user.displayName})</p>
-        <p>Отображаемая фамилия в приложении: getText({user.displayLastname})</p>
+        <p>Имя пользователя: {getText(user.username)}</p>
+        <p>Имя из телеграма: {getText(user.firstName)}</p>
+        <p>Фамилия из телеграма: {getText(user.lastName)}</p>
+        <p>Отображаемое имя в приложении: {getText(user.displayName)}</p>
+        <p>Отображаемая фамилия в приложении: {getText(user.displayLastname)}</p>
       </div>
     </div>
   );
