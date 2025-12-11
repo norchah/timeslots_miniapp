85b05e77ed01f0d40c0c10c0c435eec03bb40616
norchah <norchah@mail.ru>
2025-12-11 18:59:25 +0200

    fix

--- src/pages/BecomeProfi.jsx
+++ src/pages/BecomeProfi.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import {useTelegramNavigation} from "../hooks/useTelegramNavigation.js";
 
-export default function BecomeProfi(props) {
+export default function BecomeProfi({tgData, navigate, user}) {
+  useTelegramNavigation(tgData, {backPage: 'home', navigate})
   return (
-    <div></div>
+    <div>
+      Здесь вы станете профессионалам и начнете оказывать услуги
+      Но после небольшой регистрации
+    </div>
   );
 }
