f7b7017fd6b7e271eb443786a08776a2d7eaa63e
norchah <norchah@mail.ru>
2025-12-11 14:17:43 +0200

    fix

--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -8,7 +8,7 @@ export default function Card({children, navigate, page}) {
       }}
       className="w-full p-[2px] rounded-2xl
          bg-white/10 backdrop-blur-md
-         shadow-[0_4px_20px_-4px_rgba(255,255,255,0.4)]
+
          active:scale-[0.98] transition-all"
     >
       <div
