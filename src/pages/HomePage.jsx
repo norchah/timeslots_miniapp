d8836bc1240c2ee72a7f718dff0064510c7c5305
norchah <norchah@mail.ru>
2025-12-11 13:38:31 +0200

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ export default function HomePage({ navigate, user, safeTop, safeBottom }) {
       <h1>Добро пожаловать в TimeSlots</h1>
       <p>safe Bottom: {safeBottom}</p>
       <p>safe Top: {safeTop}</p>
-      <button onClick={() => {
+      <button className='w-[40px] h-[40px] rounded-xl bg-sky-600' onClick={() => {
         navigate('settings')
       }}>settings
       </button>
