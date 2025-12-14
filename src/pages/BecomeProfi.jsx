200bbcfa0188bdc3dd01025f4d6b2b4a8fff55bc
norchah <norchah@mail.ru>
2025-12-15 01:05:18 +0200

    fix

--- src/pages/BecomeProfi.jsx
+++ src/pages/BecomeProfi.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {useTelegramNavigation} from "../hooks/useTelegramNavigation.js";
 
-export default function BecomeProfi({tgData, navigate, user}) {
+export default function BecomeProfi({tgData, navigate}) {
   useTelegramNavigation(tgData, {backPage: 'home', navigate})
   return (
-    <div className="flex flex-col align-center justify-center w-full py-5 outline outline-red-500">
-      <h1>Становление Профессионалом</h1>
+    <div className="flex flex-col align-center justify-center py-5 outline outline-red-500">
+      <h1 className='text-3xl'>Становление Профессионалом</h1>
       <p>Здесь вы станете профессионалам и начнете оказывать услуги</p>
       <p>Но после небольшой регистрации</p>
     </div>
