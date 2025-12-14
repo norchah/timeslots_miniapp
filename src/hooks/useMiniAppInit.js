e9831396bc0c62d4aa66ce067af5190603165867
norchah <norchah@mail.ru>
2025-12-15 00:32:36 +0200

    fix

--- src/hooks/useMiniAppInit.js
+++ src/hooks/useMiniAppInit.js
@@ -16,6 +16,7 @@ export function useMiniAppInit(tgData) {
       tgData.requestFullscreen?.();
     }
 
+    setLang(tgData.language_code || 'en')
     tgData.MainButton.hide?.();
     tgData.enableClosingConfirmation?.();
 
