d6e809e0ea1d4abb3b763669830d8e186fbfa696
norchah <norchah@mail.ru>
2025-12-15 13:10:04 +0200

    fix

--- src/components/buttons/buttonMain.jsx
+++ src/components/buttons/buttonMain.jsx
@@ -6,7 +6,7 @@ export default function ButtonMain({children, navigate, page, className, type='b
       type={type}
       onClick={() => {navigate(page)}}
       className={`
-              mt-2 p-2 rounded-xl bg-blue-500 text-white
+              mt-2 py-2 px-1 rounded-xl bg-blue-500 text-white
               font-medium transition-all active:scale-[0.97]
               ${className}`}
     >{children}
