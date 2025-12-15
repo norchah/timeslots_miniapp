4fc228cf4621013f79dd48df04b7f281123d70a3
norchah <norchah@mail.ru>
2025-12-15 17:40:08 +0200

    fix

--- src/components/forms/ProfiRegistrationForm.jsx
+++ src/components/forms/ProfiRegistrationForm.jsx
@@ -18,7 +18,7 @@ export default function ProfiRegistrationForm({navigate}) {
   return (
     <form onSubmit={(e) => {
       e.preventDefault();
-      submit(values, id, navigate);
+      submit(id, navigate);
     }}>
       <TextInput
         label="Имя"
