62259d4d359cb9136fcfbe183441e2b8204013db
norchah <norchah@mail.ru>
2025-12-16 22:54:21 +0200

    fix

--- src/components/buttons/ButtonNavigate.jsx
+++ src/components/buttons/ButtonNavigate.jsx
@@ -17,7 +17,7 @@ export default function ButtonNavigate({children, page, className}) {
   return (
     <button onClick={handleClick} className={`
         mt-2 py-2 px-1 rounded-xl bg-blue-500 text-white
-        font-medium transition-all active:scale-[0.97]
+        transition-all active:scale-[0.97] text-sm
         ${className || ''}
       `}>
       {children}
