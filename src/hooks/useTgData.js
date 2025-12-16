ff39c3c669db3828666913aa5489a7be2719be46
norchah <norchah@mail.ru>
2025-12-16 15:28:10 +0200

    fix

--- src/hooks/useTgData.js
+++ src/hooks/useTgData.js
@@ -3,7 +3,7 @@ import { useI18nStore } from "../stores/useI18nStore.js";
 
 export function useTgData() {
   const setLang = useI18nStore((s) => s.setLang);
-
+  console.log('useTgData ::::::: start of useTgData')
   useEffect(() => {
     if (!window?.Telegram?.WebApp) return;
 
@@ -11,7 +11,7 @@ export function useTgData() {
 
     setLang(tg?.initDataUnsafe?.user?.language_code || 'en');
   }, []);
-
+  console.log('useTgData ::::::: end before return')
   return {
     tgData: typeof window !== "undefined"
       ? window.Telegram?.WebApp ?? null
