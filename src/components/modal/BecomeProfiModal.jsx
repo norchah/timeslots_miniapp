1b0b85618ad3248054c060034135f37a043cb102
norchah <norchah@mail.ru>
2025-12-16 17:12:47 +0200

    fix

--- src/components/modal/BecomeProfiModal.jsx
+++ src/components/modal/BecomeProfiModal.jsx
@@ -5,7 +5,7 @@ export default function BecomeProfiModal() {
   return (
     <div className="flex flex-col justify-center px-5">
       <h1 className='text-xl'>Что бы начать оказывать услуги заполните форму ниже</h1>
-      <ProfiRegistrationForm navigate={navigate}/>
+      <ProfiRegistrationForm/>
     </div>
   );
 }
