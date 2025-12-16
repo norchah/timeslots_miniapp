3d26e1fc7b08b03a9950da22be1c1afc60874d80
norchah <norchah@mail.ru>
2025-12-16 16:09:23 +0200

    fix

---
+++ src/components/modal/ProfiClientsModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ProfiClientsModal(props) {
+  return (
+    <div>Profi Clients Modals</div>
+  );
+}
--- src/pages/HomeProfiPage.jsx
+++ src/pages/HomeProfiPage.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import ButtonMain from "../components/buttons/buttonMain.jsx";
+import ButtonNavigate from "../components/buttons/ButtonNavigate.jsx";
+import ButtonOpenModal from "../components/buttons/ButtonOpenModal.jsx";
+import ProfiClientsModal from "../components/modal/ProfiClientsModal.jsx";
 
 export default function HomeProfiPage() {
   return (
     <div className='flex flex-col justify-center inline-flex items-center'>
       <h1 className='text-2xl'>Теперь ты профи</h1>
       <p>Скоро можно будет куда-то переключиться, на юзера например</p>
-      <ButtonMain
-        navigate={navigate}
+      <ButtonNavigate
         page='home'
-        tgData={tgData}
       >
         Стать юзером
-      </ButtonMain>
-      <ButtonMain
-        navigate={navigate}
-        page='profiClients'
-        tgData={tgData}
+      </ButtonNavigate>
+      <ButtonOpenModal
+        page={ProfiClientsModal}
       >
         Мои клиенты
-      </ButtonMain>
+      </ButtonOpenModal>
     </div>
   );
 }
