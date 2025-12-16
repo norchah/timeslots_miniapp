b4178d8a49ee4d137d63a50fceea32b1524dca79
norchah <norchah@mail.ru>
2025-12-16 16:14:18 +0200

    fix

--- src/pages/ProfiTagPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function ProfiTagPage(props) {
-  return (
-    <div>Тут будем менеджерить теги.</div>
-  );
-}
--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,18 +1,11 @@
 // pages/pages.js
 import HomePage from "./HomePage.jsx";
-
-import BecomeProfi from "./BecomeProfi.jsx";
 import HomeProfiPage from "./HomeProfiPage.jsx";
-import ProfiClientsPage from "./ProfiClientsPage.jsx";
 import BookingPage from "./BookingPage.jsx";
-import ProfiTagPage from "./ProfiTagPage.jsx";
 
-// Словарь всех страниц по ключу (card.href)
+// Словарь всех страниц по ключу
 export const pages = {
   home: HomePage,
   homeProfi: HomeProfiPage,
-  becomeProfi: BecomeProfi,
-  profiClients: ProfiClientsPage,
   booking: BookingPage,
-  profiTag: ProfiTagPage,
 };
