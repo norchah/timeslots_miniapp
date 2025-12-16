906ca4275064fcd608646abe362a7637c4277384
norchah <norchah@mail.ru>
2025-12-16 16:30:45 +0200

    fix

--- src/components/modal/ModalRoot.jsx
+++ src/components/modal/ModalRoot.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {useModalStore} from "../../stores/useModalStore";
+import { useModalStore } from "../../stores/useModalStore";
 import ModalWrapper from "./ModalWrapper.jsx";
 
 export default function ModalRoot() {
-  const stack = useModalStore((s) => s.stack);
+  const stack = useModalStore((s) => s.stack); // 🔹 берём стек из стора
   if (!stack.length) return null;
 
   const top = stack[stack.length - 1];
-  const ModalComponent = top.modal; // 🔑 ВАЖНО
+  const ModalComponent = top.Component; // 🔑 компонент, а не JSX
 
   return (
     <ModalWrapper>
